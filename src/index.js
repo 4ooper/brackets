32a0a7a8a8be34d456a3f4b40ed442085e006194
4ooper <79720254+4ooper@users.noreply.github.com>
2021-03-13 10:17:55 +0300

    init : start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = [];
+    let a = 0;
+    for(i=0;i<str.length;i++){
+        for(j=0;j<bracketsConfig.length;j++){
+            if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1])
+                a += 1;
+            else if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]
+                res.push(str[i]);
+            else if(str[i] === bracketsConfig[j][1]){
+                if(res.length === 0 || res[res.length - 1] !== bracketsConfig[j][0]
+                   return false;
+                res.pop();
+            }
+        }
+    }
+    return stack.length === 0 && a % 2 === 0;
 }
