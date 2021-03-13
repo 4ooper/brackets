a71756e06121377a7eab4b0a85e0f5e0b0ec0b58
4ooper <79720254+4ooper@users.noreply.github.com>
2021-03-13 10:33:48 +0300

    fix : correct some bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-    let res = [];
+    let res = []
     let a = 0;
     for(i=0;i<str.length;i++){
         for(j=0;j<bracketsConfig.length;j++){
-            if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1])
+            if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]){
                 a += 1;
-            else if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]
+            } else if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1])
                 res.push(str[i]);
             else if(str[i] === bracketsConfig[j][1]){
-                if(res.length === 0 || res[res.length - 1] !== bracketsConfig[j][0]
+                if(res.length === 0 || res[res.length - 1] !== bracketsConfig[j][0])
                    return false;
                 res.pop();
             }
